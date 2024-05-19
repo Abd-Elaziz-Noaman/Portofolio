d740f5b9b6914e8ccd5040291789cfb6dc930af1
abdelaziz-noaman <zezo.noaman@yahoo.com>
2024-05-19 04:09:46 +0300

    updated metadata

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import styles from "./page.module.css";
 import MainContainer from "@/components/MainContainer/MainContainer";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
@@ -27,20 +26,26 @@ export default function Home() {
           property="og:description"
           content="Welcome to Abd El-Aziz Noaman's portfolio. Take a look?"
         />
-        <meta property="og:image" content="/public/images/my-pic.webp" />
+        <meta
+          property="og:image"
+          content="https://abdelaziz-noaman.vercel.app/images/my-pic.webp"
+        />
         <meta
           property="og:url"
           content="https://abdelaziz-noaman.vercel.app/"
         />
         <meta property="og:type" content="website" />
 
-        <meta name="twitter:card" content="/public/images/my-pic.webp" />
+        <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Abd El-Aziz Noaman - Portfolio" />
         <meta
           name="twitter:description"
           content="Welcome to Abd El-Aziz Noaman's portfolio. Take a look?"
         />
-        <meta name="twitter:image" content="/public/images/my-pic.webp" />
+        <meta
+          name="twitter:image"
+          content="https://abdelaziz-noaman.vercel.app/images/my-pic.webp"
+        />
       </Head>
       <Suspense fallback={<Loading />}>
         <MainContainer />
