dde0437cb95758ff0fcd7c0878e760e6ac475287
abdelaziz-noaman <zezo.noaman@yahoo.com>
2024-05-19 04:02:27 +0300

    added some metadata for og and seo purpose

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import MainContainer from "@/components/MainContainer/MainContainer";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { Suspense } from "react";
+import Head from "next/head";
 
 import { ThemeContextProvider } from "../store/themeContext";
 import Loading from "./loading";
@@ -14,6 +15,33 @@ export default function Home() {
   return (
     <ThemeContextProvider>
       <CssBaseline />
+      <Head>
+        <title>Abd El-Aziz Noaman - Portfolio</title>
+        <meta
+          name="description"
+          content="Welcome to Abd El-Aziz Noaman's portfolio. Take a look?"
+        />
+
+        <meta property="og:title" content="Abd El-Aziz Noaman - Portfolio" />
+        <meta
+          property="og:description"
+          content="Welcome to Abd El-Aziz Noaman's portfolio. Take a look?"
+        />
+        <meta property="og:image" content="/public/images/my-pic.webp" />
+        <meta
+          property="og:url"
+          content="https://abdelaziz-noaman.vercel.app/"
+        />
+        <meta property="og:type" content="website" />
+
+        <meta name="twitter:card" content="/public/images/my-pic.webp" />
+        <meta name="twitter:title" content="Abd El-Aziz Noaman - Portfolio" />
+        <meta
+          name="twitter:description"
+          content="Welcome to Abd El-Aziz Noaman's portfolio. Take a look?"
+        />
+        <meta name="twitter:image" content="/public/images/my-pic.webp" />
+      </Head>
       <Suspense fallback={<Loading />}>
         <MainContainer />
       </Suspense>
